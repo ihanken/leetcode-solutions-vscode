8835511b61738b3312194545df1564ddf6cefef4
ihanken <ihanken@bellsouth.net>
2019-06-15 10:34:52 -0700

    Added number of islands

---
+++ 200.number-of-islands.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=200 lang=python3
+#
+# [200] Number of Islands
+#
+class Solution:
+    def fillIsland(self, grid: List[List[str]], i: int, j: int):
+        if grid[i][j] != '1': return
+
+        grid[i][j] = '0'
+
+        if i < len(grid) - 1: self.fillIsland(grid, i + 1, j)
+        if i > 0: self.fillIsland(grid, i - 1, j)
+        if j < len(grid[i]) - 1: self.fillIsland(grid, i, j + 1)
+        if j > 0: self.fillIsland(grid, i, j - 1)
+        
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islandCount = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    islandCount += 1
+                    self.fillIsland(grid, i, j)
+
+        return islandCount
